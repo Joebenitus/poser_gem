8a05bfbb07d89241754ba2f30be3fa92c2e5ee82
Joseph Karnafel <josephkarnafel@gmail.com>
2020-11-04 10:02:58 -0800

    refactor noun, verb, and adjective classes to contain a .all method. add passing tests for returning random nouns and verbs

--- spec/poser_spec.rb
+++ spec/poser_spec.rb
@@ -4,21 +4,30 @@ RSpec.describe Poser do
   it "has a version number" do
     expect(Poser::VERSION).not_to be nil
   end
-
-  it "returns a adjective" do
-    expect(Poser::Adjective.all).to include(p.adj)
-  end
-
+  
   it "returns a random adjective" do
     expect(Poser::Adjective.all).to include(Poser::Adjective.random)
   end
 
-  # it "returns a noun" do
-  #   expect(p.adj).to(eq('foobar'))
-  # end
-  # it "returns a noun" do
-  #   expect(p.adj).to(eq('foobar'))
-  # end
+  it "returns a random adjective using shortcut method" do
+    expect(Poser::Adjective.all).to include(p.adj)
+  end
+
+  it "returns a random noun" do
+    expect(Poser::Noun.all).to include(Poser::Noun.random)
+  end
+
+  it "returns a random noun using shortcut method" do
+    expect(Poser::Noun.all).to include(p.noun)
+  end
+
+  it "returns a random verb" do
+    expect(Poser::Verb.all).to include(Poser::Verb.random)
+  end
+
+  it "returns a verb using shortcut method" do
+    expect(Poser::Verb.all).to include(p.verb)
+  end
 
 end
 
