16079bf1a734722e699c7e661195795c0957c7fc
Joseph Karnafel <josephkarnafel@gmail.com>
2020-11-04 10:18:12 -0800

    refactor poser_spec organization and add passing test for generating random sentence

--- spec/poser_spec.rb
+++ spec/poser_spec.rb
@@ -4,7 +4,9 @@ RSpec.describe Poser do
   it "has a version number" do
     expect(Poser::VERSION).not_to be nil
   end
+end
   
+RSpec.describe Poser::Adjective do
   it "returns a random adjective" do
     expect(Poser::Adjective.all).to include(Poser::Adjective.random)
   end
@@ -12,7 +14,9 @@ RSpec.describe Poser do
   it "returns a random adjective using shortcut method" do
     expect(Poser::Adjective.all).to include(p.adj)
   end
+end
 
+RSpec.describe Poser::Noun do
   it "returns a random noun" do
     expect(Poser::Noun.all).to include(Poser::Noun.random)
   end
@@ -20,7 +24,9 @@ RSpec.describe Poser do
   it "returns a random noun using shortcut method" do
     expect(Poser::Noun.all).to include(p.noun)
   end
+end
 
+RSpec.describe Poser::Verb do
   it "returns a random verb" do
     expect(Poser::Verb.all).to include(Poser::Verb.random)
   end
@@ -28,6 +34,15 @@ RSpec.describe Poser do
   it "returns a verb using shortcut method" do
     expect(Poser::Verb.all).to include(p.verb)
   end
-
 end
 
+RSpec.describe Poser::Sentence do
+  it "returns a sentence" do
+    expect(Poser::Sentence.new).not_to be nil
+  end
+end
+
+# RSpec.describe Poser::Comment do
+#   it "returns a comment" do
+#     expect(Poser::Comment.new).not_to be nil
+#   end
